e875da069b343f5a0bdf2fa9c86ac13e79e6eb05
glfaisal <santiaiqbalfaisal.com>
2025-02-16 14:30:18 +0600

    added navigation for mobile

--- js/app.js
+++ js/app.js
@@ -6,4 +6,17 @@ $('.slider').slick({
     dotsClass:'dots'
 });
 
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+
+hamberger.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
 });
